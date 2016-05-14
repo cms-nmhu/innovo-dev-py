9bce1aca9850d692a330a014390d58b92386aba8
rmedinahu <richardmedina@CSMB01.local>
2016-05-14 08:30:23 -0600

    map work

---
+++ innovopy-site/static/js/demo.js
@@ -0,0 +1,55 @@
+
+			var width = 960,
+			    height = 960,
+			    scale0 = (width - 1) / 2 / Math.PI;
+			
+			var zoom = d3.behavior.zoom()
+			    .translate([width / 2, height / 2])
+			    .scale(scale0)
+			    .scaleExtent([scale0, 8 * scale0])
+			    .on("zoom", zoomed);
+			
+			var projection = d3.geo.mercator();
+			
+			var path = d3.geo.path().projection(projection);
+
+			var svg = d3.select("#map").append("svg")
+			    .attr("width", width)
+			    .attr("height", height)
+			  	.append("g");
+
+			var g = svg.append("g");
+			svg.append("rect")
+			    .attr("class", "overlay")
+			    .attr("width", width)
+			    .attr("height", height);
+			svg
+			    .call(zoom)
+			    .call(zoom.event);
+			
+			d3.json("/static/data/world-110m.json", function(error, world) {
+				if (error) throw error;
+				g.append("path")
+				  .datum({type: "Sphere"})
+				  .attr("class", "sphere")
+				  .attr("d", path);
+				g.append("path")
+				  .datum(topojson.merge(world, world.objects.countries.geometries))
+				  .attr("class", "land")
+				  .attr("d", path);
+				g.append("path")
+				  .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
+				  .attr("class", "boundary")
+				  .attr("d", path);
+			});
+			
+			function zoomed() {
+			  console.log(zoom.scale());
+			  projection
+			      .translate(zoom.translate())
+			      .scale(zoom.scale());
+			  g.selectAll("path")
+			      .attr("d", path);
+			}
+			d3.select(self.frameElement).style("height", height + "px");
+		
--- innovopy-site/static/js/net.js
+++ innovopy-site/static/js/net.js
@@ -1,11 +1,3 @@
-var width = 800,
-    height = 600;
-
-var svg = d3.select(".content").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("stroke", "steelblue");
-
 var force = d3.layout.force()
     .gravity(0.05)
     .distance(200)
@@ -15,10 +7,10 @@ var force = d3.layout.force()
 d3.json("/static/data/graph.json", function(error, json) {
     if (error) throw error;
 
-    force
-        .nodes(json.nodes)
-        .links(json.links)
-        .start();
+    // force
+    //     .nodes(json.nodes)
+    //     .links(json.links)
+    //     .start();
 
     var link = svg.selectAll(".link")
         .data(json.links)
@@ -32,7 +24,7 @@ d3.json("/static/data/graph.json", function(error, json) {
         .attr("class", "node")
         .attr("width", 200)
         .attr("height", 300)
-        .call(force.drag);
+        ;//.call(force.drag);
 
         
 
@@ -40,8 +32,8 @@ d3.json("/static/data/graph.json", function(error, json) {
     //     .attr("r", function(d) { return d.mag });
 
     node.append("foreignObject")
-        .attr("x", -12)
-        .attr("y", -20)
+        // .attr("x", -12)
+        // .attr("y", -20)
         .attr("width",  100)
         .attr("height", 100)
         .attr("cx", function(d) { return projection([d.lon, d.lat])[0] })
@@ -50,12 +42,12 @@ d3.json("/static/data/graph.json", function(error, json) {
             return "<i class='fa fa-2x fa-"+d.type+"'></i> " + d.name
         });
 
-    force.on("tick", function() {
-        link.attr("x1", function(d) { return d.source.x; })
-          .attr("y1", function(d) { return d.source.y; })
-          .attr("x2", function(d) { return d.target.x; })
-          .attr("y2", function(d) { return d.target.y; });
+    // force.on("tick", function() {
+    //     link.attr("x1", function(d) { return d.source.x; })
+    //       .attr("y1", function(d) { return d.source.y; })
+    //       .attr("x2", function(d) { return d.target.x; })
+    //       .attr("y2", function(d) { return d.target.y; });
 
-        node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-    });
+    //     node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+    // });
 });
