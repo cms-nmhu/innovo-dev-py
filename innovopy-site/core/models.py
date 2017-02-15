f165053dc066c9edded3100bc8a304d803b17112
rmedinahu <rmedina@nmhu.edu>
2017-02-15 15:39:22 -0700

    additions to innovo user

--- innovopy-site/core/models.py
+++ innovopy-site/core/models.py
@@ -3,6 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
+DEGREE_TYPES = (
+    (0, 'No Degree'),
+    (1, 'Associates'),
+    (2, 'Bachelors'),
+    (3, 'Masters'),
+    (4, 'Doctorate'),
+)
 
 class DocumentFile(models.Model):
     file = models.FileField(null=True)
@@ -12,10 +19,15 @@ class DocumentFile(models.Model):
 
 class InnovoUser(models.Model):
     user = models.OneToOneField(User)
-    photo = models.ImageField(null=True)
+    degree_type = models.IntegerField(choices=DEGREE_TYPES, blank=True, default=0)
+    degree = models.CharField(max_length=512, blank=True, null=True)
+    affiliation = models.CharField(max_length=512, blank=True, null=True)
+    prev_affiliations = models.TextField(blank=True, null=True)
+    prev_positions = models.TextField(blank=True, null=True)
+    photo = models.ImageField(blank=True, null=True)
 
     def __unicode__(self):
-        return self.user
+        return self.user.username
 
 
 class Publication(models.Model):
