87e5616d903e274a792e2a18788fa289a10a4987
ejucov <ejucov@live.nmhu.edu>
2017-05-01 14:49:25 -0600

    new

--- innovopy-site/core/search_indexes.py
+++ innovopy-site/core/search_indexes.py
@@ -7,10 +7,11 @@ from asset.models import Asset
 class AssetIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     title = indexes.CharField(model_attr='title')
-    room = indexes.CharField(model_attr='room')
-    short_desc = indexes.CharField(model_attr='short_desc')
-    full_desc = indexes.CharField(model_attr='full_desc')
-    contact_1_name = indexes.CharField(model_attr='contact_1_name')
+    room = indexes.CharField(model_attr='room', default='', faceted=True)
+    short_desc = indexes.CharField(model_attr='short_desc', default='')
+    full_desc = indexes.CharField(model_attr='full_desc', default='')
+    contact_1_name = indexes.CharField(model_attr='contact_1_name', default='')
+    manufacturer = indexes.CharField(model_attr='manufacturer', default='')
 
 
     def get_model(self):
