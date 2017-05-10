d00e0ee29abd197b6e99a2c3169a4f2d018719c9
rmedinahu <rmedina@nmhu.edu>
2017-05-10 17:01:50 -0600

    haystack implemented

--- innovopy-site/innovopy/urls.py
+++ innovopy-site/innovopy/urls.py
@@ -48,7 +48,7 @@ urlpatterns = [
     
     url(r'^admin/', admin.site.urls),
 
-    url(r'^__debug__/', include(debug_toolbar.urls)),
+    # url(r'^__debug__/', include(debug_toolbar.urls)),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
