70c70e471625f4a5e7bc05c29e0dc1bf6a6786e3
rmedinahu <rmedina@nmhu.edu>
2017-05-11 15:15:54 -0600

    innovouser admin

--- innovopy-site/core/admin.py
+++ innovopy-site/core/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 
 from .models import DocumentFile, InnovoUser, Category, InnovoTag, CpvCode
 
+class InnovoUserAdmin(admin.ModelAdmin):
+    list_display = ('user', 'degree_type', 'degree', 'affiliation')
+    list_editable = ['degree_type', 'degree']
+    list_filter = ('degree_type', 'degree')
+
+
 admin.site.register(DocumentFile)
-admin.site.register(InnovoUser)
+admin.site.register(InnovoUser, InnovoUserAdmin)
 admin.site.register(Category)
 admin.site.register(InnovoTag)
 admin.site.register(CpvCode)
